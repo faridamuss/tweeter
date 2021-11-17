b20fd1b47a1f24ecb33a41f292678291c6ba3e70
Farida <faridamus@protonmail.com>
2021-11-17 00:19:26 +0000

    Implement the character counter and counter turns red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
- 
-  $('#tweet-text').on('keyup', function(e) {
-    console.log($("#tweet-text").val().length)
-    console.log(this); 
-  })
- 
- 
+  $('textarea').keyup(function () {
+    let maxLength = 140;
+      let textLength = maxLength - $(this).val().length;
+      let $output = $('output')
+      $output.text(textLength);
+      if (textLength < 0) {
+          $output.addClass('invalid')
+      } else {
+          $output.removeClass('invalid')
+      }
+  });
 });
