629c204ed04ccd36a95c081c08f6c9ea36a1704b
Farida <faridamus@protonmail.com>
2021-11-18 19:59:09 +0000

    Implement renderTweets function that take in createTweetElementfunction

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,40 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-//implement a createTweetElement function that takes in a tweet object and returns an HTML
 $(document).ready(function() {
+
+const data = [
+  {
+    "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+const renderTweets = function(tweets) {
+    tweets.forEach((tweet) => {
+      const tweetNode = createTweetElement(tweet);
+      $('#tweets-container').prepend(tweetNode);
+            // loops through tweets
+            // calls createTweetElement for each tweet
+            // takes return value and appends it to the tweets container
+    });
+}
+  
 function createTweetElement(tweetData) {
   const $tweet = $(`<article class="tweet">
   <header class="th-header">
@@ -32,25 +61,6 @@ function createTweetElement(tweetData) {
  )
  return $tweet;
 }
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log("Tweet is here:", $tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 
 });
