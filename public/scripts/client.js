6a76cd7c7caebf7ea289d8d897f94cf1924cf56a
Farida <faridamus@protonmail.com>
2021-11-18 22:48:29 +0000

    Add an Event Listener and Prevent the Default Behaviour

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,39 +1,9 @@
-$(document).ready(function() {
-
-const data = [
-  {
-    "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
 const renderTweets = function(tweets) {
-    tweets.forEach((tweet) => {
-      const tweetNode = createTweetElement(tweet);
-      $('#tweets-container').prepend(tweetNode);
-            // loops through tweets
-            // calls createTweetElement for each tweet
-            // takes return value and appends it to the tweets container
-    });
-}
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').prepend($tweet);
+  }
+};
   
 function createTweetElement(tweetData) {
   const $tweet = $(`<article class="tweet">
@@ -50,7 +20,7 @@ function createTweetElement(tweetData) {
   <p>${tweetData.content.text} </p>
 </div> 
 <footer class="tweet-footer">
-<span class="time-passed">${timeago.format(new Date())}</span> 
+<span class="time-passed">${timeago.format(tweetData.created_at)}</span> 
   <div class="icons">
     <i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i>
@@ -61,6 +31,23 @@ function createTweetElement(tweetData) {
  )
  return $tweet;
 }
-renderTweets(data);
 
-});
+const loadTweets = () => {
+  $.ajax({
+    url:"/tweets", 
+    method:'GET', 
+  })
+  .then((datatweets) => {
+    renderTweets(datatweets); 
+  })
+}
+
+$(document).ready(function() {
+  loadTweets()
+  $("#post-tweet").submit(function(event){
+    event.preventDefault();
+    console.log("New tweeter!")
+  });
+})
+
+
