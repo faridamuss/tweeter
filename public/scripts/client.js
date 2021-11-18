9bc823496b03ccf122dbe2f8b331638e14f63c5e
Farida <faridamus@protonmail.com>
2021-11-18 02:38:36 +0000

    Updating html and linting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-createTweetElement 
+//implement a createTweetElement function that takes in a tweet object and returns an HTML 
+
+function createTweetElement(tweetData) {
+
+
+
+};
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
