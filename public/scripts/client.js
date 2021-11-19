fe97464cfb0d31efe6ad8091be940989eb01afd8
Farida <faridamus@protonmail.com>
2021-11-19 04:07:24 +0000

    Lint functions before submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@ $('.scroll').click(() => {
 });
 
 //function to build html for tweets dynamically
-const createTweetElement = function (tweetObj) {
+const createTweetElement = function(tweetObj) {
   const $tweet = $("<article class='tweet'>");
   //tweet header
   const $header = $("<header class='th-header'>");
@@ -25,10 +25,10 @@ const createTweetElement = function (tweetObj) {
   //append tweet text to container
   $contentContainer.append($contentText);
   //tweet footer
-  const $footer = $("<footer>")
+  const $footer = $("<footer>");
   //tweet footer children
   const timeDelta = moment(tweetObj.created_at).fromNow();
-  const $timeStamp = $("<p>").text(timeDelta);;
+  const $timeStamp = $("<p>").text(timeDelta);
   const $footerD2 = $("<div class='icons'>");
   const $flag = $("<i class='far fa-flag' id='flag'>");
   const $retweet = $("<i class='fas fa-retweet' id='retweet'>");
@@ -42,14 +42,14 @@ const createTweetElement = function (tweetObj) {
 };
 
 //loops through all tweets
-const renderTweets = function (tweets) {
-  const $container = $('#tweets-container')
+const renderTweets = function(tweets) {
+  const $container = $('#tweets-container');
   $container.empty();
   tweets.forEach((tweet) => {
     const tweetNode = createTweetElement(tweet);
     $container.prepend(tweetNode);
   });
-}
+};
 
 const loadTweets = () => {
   $.ajax({
@@ -68,24 +68,24 @@ const loadTweets = () => {
 //function to add custom error message
 const appendError = (message) => {
   $('#submit-tweet').prepend($("<span class='error'>").text(' ⚠️ ' + message + ' ⚠️').slideDown().delay(3500).hide(500));
-}
+};
 
 //removes errors to keep multiple errors from popping up with repeated error inducing clicks
 const removeError = () => {
-  $('.error').remove()
-}
+  $('.error').remove();
+};
 
 //resets countdoen to 140 characters
 const resetCounter = () => {
   $('.counter').text(140);
-}
+};
 
 //handles submit click and employs helper functions from above
-$(document).ready(function () {
+$(document).ready(function() {
   loadTweets();
   //form submit handler
   const $submitTweet = $('#submit-tweet');
-  $submitTweet.on('submit', function (e) {
+  $submitTweet.on('submit', function(e) {
     e.preventDefault();
     const serializedData = $(this).serialize();
     //handle errors
@@ -93,7 +93,7 @@ $(document).ready(function () {
     if ($('#tweet-text').val() === '' || null) {
       appendError("You cannot post a blank tweet");
     } else if ($('#tweet-text').val().length > 140) {
-      appendError("Your tweet is too long!")
+      appendError("Your tweet is too long!");
     } else {
       //post tweets
       $.post('/tweets', serializedData)
@@ -101,7 +101,7 @@ $(document).ready(function () {
           loadTweets();
           $(this).children('textarea').val('');
           resetCounter();
-        })
+        });
     }
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,17 @@
+//responsible for character counter of tweet input form
 $(document).ready(function() {
-  $('textarea').keyup(function () {
-    const maxLength = 140;
-      const textLength = maxLength - $(this).val().length;
-      const $output = $('output')
-      $output.text(textLength);
-      if (textLength < 0) {
-          $output.addClass('invalid')
-      } else {
-          $output.removeClass('invalid')
-      }
+  $('#tweet-text').on('keyup', function(e) {
+    //tracking character count input from user using keyup
+    const currCharCount = $(this).val().length;
+    //locating the counter element
+    const counterElem = $(this).parent().children('.under-tweet-input').children('.counter');
+    const remainingChars = 140 - currCharCount;
+    //updating counter elem on DOM to reflect current changes in remaining characters
+    counterElem.text(remainingChars);
+    if (remainingChars < 0) {
+      counterElem.css('color', 'mediumvioletred');
+    } else {
+      counterElem.css('color', '#545149');
+    }
   });
 });
